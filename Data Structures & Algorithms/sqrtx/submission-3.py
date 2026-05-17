fdd04dcbc633510d1005b89a6e5c7c8cebaaa9e5
Victoria Zhang <31415280+ZhanqiZhang66@users.noreply.github.com>
2026-05-17 00:13:05 -0700

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x <= 0:
+            return 0
+        if x <= 2:
+            return x
+        l, r = 1, x
+        # find first mid s.t mid **2 >= x
+        while l < r:
+            mid  = (l + r) // 2
+            if mid * mid > x:
+                r = mid
+            else:
+                l = mid + 1
+        return l - 1 
