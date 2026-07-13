4c2a8b8daff967604e1a100ae8cf9b57d9d1bbd2
Victoria Zhang <31415280+ZhanqiZhang66@users.noreply.github.com>
2026-07-12 21:01:54 -0700

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # UnionFind
+        class UnionFind:
+            def __init__(self, n):
+                # parent : what is the root of a node
+                self.parent = list(range(n))
+                # sizes of each components 
+                self.sizes = [1] * n
+            def find(self, x):
+                
+                # this node has a different node as parent
+                if x != self.parent[x]:
+                    self.parent[x] = self.find(self.parent[x])
+                # this node is its own parent
+                return self.parent[x]
+            def union(self, x, y):
+                # 1. root (x) == root(y)
+                rx, ry = self.find(x), self.find(y)
+                if rx == ry:
+                    return False
+                if rx != ry:
+                    if self.sizes[rx] < self.sizes[ry]:
+                        rx, ry = ry, rx
+                    # merge root(y ) into x
+                    self.parent[ry] = rx
+                    self.sizes[rx] += self.sizes[ry]
+                return True
+        c = n
+        uf = UnionFind(n)
+        for a, b in edges:
+            if uf.union(a, b):
+                c -= 1
+        # for each node, union them if they are in the same graph, 
+        return c 
+        
